8b9f284c4567a1d5b30450a09745318fcb8647ed
Dina Morales <dinamrivera08@gmail.com>
2026-02-13 09:23:40 -0600

    update stock

--- src/data/productshistoric2025.js
+++ src/data/productshistoric2025.js
@@ -451,6 +451,119 @@ Es una taza cómoda, robusta y visualmente única para disfrutar de tu bebida fa
     ],
   },
   {
+    id: "13",
+    title: "Salserito 'Tierra y Agua'",
+    price: 90.0,
+    stock: 0,
+    description: `
+Salserito con perfil bajo y un práctico pico vertedor. Esta pieza ha sido modelada a mano con técnica de pellizco y presenta la calidez de la arcilla roja expuesta en su base.
+
+Su esmalte es una mezcla de tonos aqua, azul y potentes blanco, creando. Es ideal para servir salsas, aderezos o para degustaciones.
+    `,
+    details: [
+      "Material: Arcilla roja",
+      "Acabado: Esmalte fluido con colores aqua, azul y translúcido",
+      "Medidas: 1.7 pulgadas de alto × 4.2 pulgadas de diámetro",
+    ],
+    slug: "salserito-tierra-y-agua",
+    mainImage: "/piezas/Pieza13_1.jpg",
+    gallery: [
+      "/piezas/Pieza13_1.jpg",
+      "/piezas/Pieza13_2.jpg",
+      "/piezas/Pieza13_3.jpg",
+      "/piezas/Pieza13_4.jpg",
+    ],
+  },
+  {
+    id: "11",
+    title: "Pichelito 'Gold Breeze'",
+    price: 180.0,
+    stock: 0,
+    description: `
+Una pieza funcional y con carácter, ideal como cremera, jarra de leche o para servir cualquier aderezo. Su diseño cilíndrico ha sido modelado en torno e incluye un pico vertedor.
+
+El esmalte combina azules profundos y blancos suaves con manchas doradas y óxido.     `,
+    details: [
+      "Material: Arcilla roja",
+      "Uso: Cremera, jarra de leche/aderezos, salserito.",
+      "Acabado: Esmalte con tonos azules y acentos óxido/dorados",
+      "Medidas: 4 pulgadas de alto × 3.3 pulgadas de diámetro",
+    ],
+    slug: "pichelito-gold-breeze",
+    mainImage: "/piezas/Pieza11_1.jpg",
+    gallery: ["/piezas/Pieza11_1.jpg", "/piezas/Pieza11_2.jpg"],
+  },
+  {
+    id: "19",
+    title: "Bowl de Cereal 'Snow'",
+    price: 100,
+    oldPrice: 120,
+    stock: 0,
+    description: `
+Bowl profundo, ideal para servir cereales, yogurt, berries, sopas o guisos. Modelado a mano con torno, presenta una forma sencilla y rústica.
+
+Su esmalte crea un efecto de nieve, fusionando tonos blanco, azules claros y oscuros. Se percibe en en ciertas areas la arcilla roja bajo un esmalte translúcido.
+    `,
+    details: [
+      "Material: Arcilla roja",
+      "Acabado: Esmalte blanco con tonos azules claros y oscuros.",
+      "Medidas: 2.5 pulgadas de alto × 5.5 pulgadas de diámetro",
+    ],
+    slug: "bowl-cereal-snow",
+    mainImage: "/piezas/Pieza19_4.jpg",
+    gallery: ["/piezas/Pieza19_4.jpg", "/piezas/Pieza19_6.jpg"],
+  },
+  {
+    id: "22",
+    title: "'Seaside' Mug (Sin Asa)",
+    price: 185,
+    stock: 0,
+    description: `Taza modelada en arcilla tono vainilla, con un diseño minimalista sin asa tipo tumbler. Su acabado en esmalte celeste se funde suavemente con la base color arena, evocando la calma de un paisaje costero. Una pieza versátil e ideal tanto para bebidas frías como calientes.`,
+    details: [
+      "Material: Arcilla color vainilla",
+      "Acabado: Esmalte color turquesa por dentro. Por fuera el esmalte de base es color arena y se sobrelapa con el turquesa que predomina en la parte superior.",
+      "Capacidad: 350ml",
+    ],
+    slug: "seaside-mug",
+    mainImage: "/piezas/Dec2025/Kahlu_ID22_00003.jpg",
+    gallery: [
+      "/piezas/Dec2025/Kahlu_ID22_00001.jpg",
+      "/piezas/Dec2025/Kahlu_ID22_00003.jpg",
+      "/piezas/Dec2025/Kahlu_ID22_00004.jpg",
+      "/piezas/Dec2025/Kahlu_ID22_00002.jpg",
+      "/piezas/Dec2025/Kahlu_ID22_00005.jpg",
+    ],
+  },
+  {
+    id: "7",
+    title: "Salsero 'Tierra y Agua'",
+    price: 120.0,
+    oldPrice: 150,
+    stock: 0,
+    description: `
+Una pieza funcional de cerámica, ideal para servir salsas, cremas o aderezos con un toque de distinción. Modelada a mano con el torno, en arcilla roja oscura, tiene un cómodo pico vertedor.
+
+El exterior deja ver el tono natural y terroso de la arcilla, mientras que el interior está cubierto por un esmalte semi translucido que combina azules profundos y toques aqua. Un contraste elegante entre tierra y agua.
+Esmaltada con un azul celeste que crea un efecto 'speckled' sobre la arcilla oscura, dejando la base expuesta para un hermoso contraste.
+    `,
+    details: [
+      "Material: Arcilla roja",
+      "Acabado: Esmalte semi translúcido con destellos azules y aqua.",
+      "Uso: Salsero, cremero o jarrita para aderezos.",
+      "Medidas: 2.5 pulgadasde alto × 4.5 pulgadas de diámetro",
+    ],
+    slug: "salsero-tierra-y-agua",
+    mainImage: "/piezas/Pieza5_1.jpg",
+    gallery: [
+      "/piezas/Pieza5_1.jpg",
+      "/piezas/Pieza5_2.jpg",
+      "/piezas/Pieza5_3.jpg",
+      "/piezas/Pieza5_4.jpg",
+      "/piezas/Pieza5_5.jpg",
+      "/piezas/Pieza5_6.jpg",
+    ],
+  },
+  {
     id: "18",
     title: "Plato de cena 'Snow'",
     price: 170,
